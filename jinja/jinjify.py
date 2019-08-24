3a03666664d4077471a20086f0b647de3a271e0a
James Vasile <james@jamesvasile.com>
2019-08-24 03:33:49 -0400

    WIP: redactions
    It works, but needs some cleanup and documentation

--- jinja/jinjify.py
+++ jinja/jinjify.py
@@ -19,7 +19,12 @@ import click
 import frontmatter
 import jinja2
 import subprocess
+import re
 import sys
+
+regex = {'sub':r'[^a-zA-Z0-9.,?!]'}
+for r in regex:
+    regex[r] = re.compile(regex[r])
 
 # We're not using this func right now, but I do want to add markdown
 # support back in later, once I've figure out how to fit it in
@@ -34,10 +39,68 @@ def md2tex(string):
     )
     return p.communicate(input=string.encode('UTF-8'))[0].decode('UTF-8') 
 
+def redact(args, text):
+    """Go through the list of strings in 'redacted' YAML field.  Treat
+each one as a regex and replace each regex with a LaTeX function that
+prints a censored black box.
+
+    """
+    if not 'redact' in args:
+        return text
+    
+    ## Make a list of places we should censor, taking care to handle
+    ## overlapping matches (we don't want a short match to prevent a
+    ## long match that is a superset to not match)
+    censor = []
+    for pat in args['redacted']:
+        pat = pat.replace(r'\ ', '\s*')
+        pat = re.compile(pat, re.IGNORECASE)
+        for m in pat.finditer(text):
+            span = m.span()
+            for i in range(len(censor)):
+                c = censor[i]
+                if (span and
+                    span[0] < c[1] and
+                    span[1] > c[0]):
+                    censor[i] = (min(c[0], span[0]), max(c[1], span[1]))
+                    span = None
+            if span:
+                censor.append(span)
+
+    # Replace any matches with censored X boxes
+    censor = sorted(censor)
+    retext = ""
+    last = 0
+    for c in censor:
+        retext += text[last:c[0]] + r"\censor{XXXX}"
+        last = c[1]
+    text = retext + text[last:]
+
+    
+    # Don't censor in the acronym definition section.  It breaks
+    # LaTeX.  Just remove the line instead.
+    state = None
+    lines = text.split("\n")
+    for l in range(len(lines)):
+        line = lines[l].strip()
+        if "Legacy" in line:
+            print(line)
+        if line.startswith("\\begin{acronym}"):
+            state = "in acronyms"
+        elif state == "in acronyms" and "\\censor{" in line:
+                lines[l] = ""
+        elif line.startswith("\end{acronym}"):
+            state = "done"
+    text = "\n".join(lines)
+    
+    return text
+
 @click.command()
 @click.argument('filename')
 @click.option('--option', '-o', nargs=2, multiple=True, default='', help='Variables and their values.')
-def jinjify(filename, option):
+@click.option('--output')
+#@click.option('--redact', 'redacted', flag_value=True, default=False)
+def jinjify(filename, option, output):
     """Read and parse yaml+latex file."""
     doc = frontmatter.load(filename)
     template = doc.content # treat latex as a jinja2 template
@@ -49,7 +112,7 @@ def jinjify(filename, option):
     # Pass to jinja any options submitted on the command line
     for o in option:
         args[o[0]] = o[1]
-        
+
     # Make a version of a Jinja2 parser that plays nice with LaTeX
     latex_jinja_env = jinja2.Environment(
             block_start_string = '\BLOCK{',
@@ -78,6 +141,12 @@ def jinjify(filename, option):
     #template.globals.update(testfunc=testfunc)
 
     rendered = template.render(**args)
-    print(rendered)
+    rendered = redact(args, rendered)
+
+    if output:
+        with open(output, 'w') as fh:
+            fh.write(rendered)
+    else:
+        print(rendered)
 if __name__ == '__main__':
     jinjify()
